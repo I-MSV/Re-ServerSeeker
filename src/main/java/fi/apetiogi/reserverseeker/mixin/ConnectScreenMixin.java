b66b489b66f9c51c72caa35ce6adfe935a3dc6cc
Ivan-Msv <s2200853@edu.bc.fi>
2025-06-23 03:35:50 +0300

    Fixed force disconnect button

--- src/main/java/fi/apetiogi/reserverseeker/mixin/ConnectScreenMixin.java
+++ src/main/java/fi/apetiogi/reserverseeker/mixin/ConnectScreenMixin.java
@@ -3,6 +3,7 @@ package fi.apetiogi.reserverseeker.mixin;
 import javax.annotation.Nullable;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -19,10 +20,14 @@ import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.client.network.CookieStorage;
 import net.minecraft.client.network.ServerAddress;
 import net.minecraft.client.network.ServerInfo;
+import net.minecraft.network.ClientConnection;
 import net.minecraft.text.Text;
 
 @Mixin(ConnectScreen.class)
 public abstract class ConnectScreenMixin extends Screen {
+    @Shadow
+    volatile ClientConnection connection;
+
     @Unique
     private ButtonWidget deleteButton;
 
@@ -46,27 +51,15 @@ public abstract class ConnectScreenMixin extends Screen {
         MoreButtons module = Modules.get().get(MoreButtons.class);
         if (!module.isActive() || !module.enableCancelOverride.get()) return;
 
-        ConnectScreenAccessor access = (ConnectScreenAccessor)(Object)this;
-
-        // Assume connect screen only has one button
+        //Assume connect screen only has one button
         ButtonWidget cancelButton = (ButtonWidget)this.children().get(0);
         ButtonWidget overrideButton = new ButtonWidget.Builder(
             Text.literal("Force disconnect"),
             onPress -> {
-                access.setConnectingCancelled(true);
+                //I genuinely don't even know why this exists atp
+                //Could add some text here one day
                 Text title_text = Text.literal("Force disconnected");
-                // String disconnected_from = String.format("Disconnected from: %s", targetServer.name);
-                // String ip_address = String.format("Ip: %s", targetServer.address);
-                // String server_status = "Server status: offline";
-                // if (targetServer.players != null) {
-                //     server_status = String.format("Server status: %d/%d", targetServer.players.online(), targetServer.players.max());
-                // }
-
-                // Text reason_text = Text.literal(String.format("%s\n%s\n%s", disconnected_from, ip_address, server_status));
-                Text reason_text = Text.literal("");
-                
-                // dont rly like how it looks, and it doesnt provide you with any actual information
-                this.client.setScreen(new DisconnectedScreen(new MultiplayerScreen(null), title_text, reason_text));
+                this.connection.disconnect(title_text);
             }
         )
         .position(cancelButton.getX(), cancelButton.getY())
